b5a0c1f53153f93d74cb5f83ff47119fb30bc543
jeyberg <and.joh@hotmail.de>
2024-09-10 15:56:59 +0200

    add(*): new form components

---
+++ src/character.creation/form.container/form.container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form.container',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.container.component.html',
+  styleUrl: './form.container.component.sass'
+})
+export class FormContainerComponent {
+
+}
---
+++ src/character.creation/steps.indicator/steps.indicator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-steps.indicator',
+  standalone: true,
+  imports: [],
+  templateUrl: './steps.indicator.component.html',
+  styleUrl: './steps.indicator.component.sass'
+})
+export class StepsIndicatorComponent {
+
+}
