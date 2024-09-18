c356a803a960373353a4be15134baa19d38a0953
jeyberg <and.joh@hotmail.de>
2024-09-18 18:35:39 +0200

    add(app.routes): default route to form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormContainerComponent } from '../character.creation/form.container/form.container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FormContainerComponent }
+];
