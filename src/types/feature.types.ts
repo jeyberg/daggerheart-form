90ef326df5743e59d37bb258f50bd6555d8fb4d5
jeyberg <and.joh@hotmail.de>
2024-10-18 15:05:25 +0200

    fix(feature.types.ts): linting

--- src/types/feature.types.ts
+++ src/types/feature.types.ts
@@ -1,22 +1,22 @@
-import { CharacterClass, CharacterSubclass } from "./class"
-import { Ancestry, Community } from "./heritage"
-import { Armor, Item, Weapon } from "./items"
+import { CharacterClass, CharacterSubclass } from './class';
+import { Ancestry, Community } from './heritage';
+import { Armor, Item, Weapon } from './items';
 
 export interface Feature {
-    id: number,
-    name?: string,
-    description: string,
-    effects: string[],
-    appendix?: string
+  id: number;
+  name?: string;
+  description: string;
+  effects: string[];
+  appendix?: string;
 
-    community?: Community,
-    ancestry?: Ancestry,
-    ancestryId?: string,
-    item?: Item,
-    weapon?: Weapon,
-    class?: CharacterClass,
-    classId?: string,
-    subClass?: CharacterSubclass,
-    subClassId?: string,
-    armor?: Armor
+  community?: Community;
+  ancestry?: Ancestry;
+  ancestryId?: string;
+  item?: Item;
+  weapon?: Weapon;
+  class?: CharacterClass;
+  classId?: string;
+  subClass?: CharacterSubclass;
+  subClassId?: string;
+  armor?: Armor;
 }
