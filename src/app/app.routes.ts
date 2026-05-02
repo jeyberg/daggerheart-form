42e90b5a2e1f7e905957e2b4756abd54398b22c4
jeyberg <and.joh@hotmail.de>
2026-05-02 21:25:08 +0200

    idk what this is anymore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CharacterCreationContainerComponent } from './character-creation/character-creation-container/character-creation-container.component';
+import { SheetOverviewComponent } from './home/sheet-overview/sheet-overview.component';
 
 export const routes: Routes = [
-    { path: '', component: AppComponent },
+    { path: '', component: SheetOverviewComponent },
     { path: 'character-creation', component: CharacterCreationContainerComponent}
 ];
